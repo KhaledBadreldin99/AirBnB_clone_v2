ea06e920b97722d516e6253b74866a70e9ecf2c8
KhaledBadreldin99 <khaled.badr24.kb@gmail.com>
2024-03-18 04:54:54 -0700

    gV

--- models/place.py
+++ models/place.py
@@ -1,8 +1,43 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""Contains the class place."""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlachemy
+from sqlachemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+if models.storage_t == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                  ForeignKey('places.id', onupdate='CASCADE',
+                                             ondelete='CASCADE'),
+                                  primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True))
+
 
 class place(BaseModel):
     """Class representing a place"""
+    if models.storage_t == 'db':
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id =  Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(string(128), nullable=False)
+        description = Column(string(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms =  Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                  backref="place_amenities",
+                                  viewonly=False)
+    else:
 
         name = ""
         city_id = ""
@@ -19,3 +54,15 @@ class place(BaseModel):
         def __init__(self, *args, **kwargs):
             """ constructor method """
             super().__init__(self, *args, **kwargs)
+
+        if models.storage_t != 'db':
+            @property
+            def reviews(self):
+                """The getter attitude provides a list of Review instances."""
+                from models.review import Review
+                review_list = []
+                all_reviews = models.storage.all(Review)
+                for review in all_reviews.values():
+                    if review.place_id == self.id:
+                        review_list.append(review)
+                return review_list
