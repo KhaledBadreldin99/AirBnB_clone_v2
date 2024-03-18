c8b53fc733a29779cbc59493c7d0d20ef6097f62
KhaledBadreldin99 <khaled.badr24.kb@gmail.com>
2024-03-18 04:14:52 -0700

    4r

--- models/user.py
+++ models/user.py
@@ -3,17 +3,34 @@
 Module for user class
 
 """
-from models.base_model import BaseModel
-import json
+import models
+from models.base_model import BaseModel, Base
+import os import getenv
+import sqlalchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, string
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     """
-    base model class
+    User representation
 
     """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self, *args, **kwargs):
+        """
+        Creates a user instance.
+        """
+        super().__init__(*args, **kwargs)
